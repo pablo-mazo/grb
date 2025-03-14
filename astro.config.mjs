765a5d45dc764c5a748f09ec71f1f98cf3196601
Pablo Mazo <jpjm1998@hotmail.com>
2025-03-13 23:39:16 -0500

    Add standalone server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,34 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
-    server: {
-        host: true, // Allow connections from all addresses
-        port: 4322,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
-    },
-    vite: {
-        server: {
-            strictPort: true,
-            hmr: {
-                clientPort: 443
-            },
-            allowedHosts: [
-                'localhost',
-                '*.trycloudflare.com'
-            ]
-        }
-    }
+  integrations: [icon()],
+
+  server: {
+      host: true, // Allow connections from all addresses
+      port: 4321,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      }
+  },
+
+  vite: {
+      server: {
+          strictPort: true,
+          hmr: {
+              clientPort: 443
+          },
+          allowedHosts: [
+              'localhost',
+              '*.trycloudflare.com'
+          ]
+      }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
